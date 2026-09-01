821fe638e3eaba0cdadbdaa9aec2e1dc044ecaad
Nashid <nashid@streamly.app>
2026-09-01 12:09:21 +0530

    Remove obsolete scratch files

--- scratch-test-st.mjs
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-const API_URL = "https://streamly-backend-9q7i.onrender.com/api";
-
-async function test() {
-  const res = await axios.get(`${API_URL}/movies/tmdb-tv-66732?platform=netflix&_cb=v2`);
-  console.log(JSON.stringify(res.data, null, 2));
-}
-test().catch(console.error);
